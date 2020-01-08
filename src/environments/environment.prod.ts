543a24e8376961220df02b901cadd824c442e126
Andres Felipe Torres <andres.torres@ceiba.com.co>
2020-01-08 11:05:40 -0500

    Clase 94: Ajuste a la estructura de tabs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apikey: '56f1a53575784211aecad19933a18012'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apikey: '56f1a53575784211aecad19933a18012'
 };
 
 /*
