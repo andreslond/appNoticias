790d39dacb1c8ec392373ba5f59e5b3bbe206262
Andres Felipe Torres <andres.torres@ceiba.com.co>
2020-01-15 13:15:16 -0500

    Clase 106: Opción de Action sheet para comapartir en android e IOS

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Article } from 'src/app/Interfaces/interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { ActionSheetController } from '@ionic/angular';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 @Component({
   selector: 'app-noticia',
@@ -15,7 +16,8 @@ export class NoticiaComponent implements OnInit {
 
   constructor(
     private inAppBrowser: InAppBrowser,
-    public actionSheetController: ActionSheetController) { }
+    public actionSheetController: ActionSheetController,
+    private socialSharing: SocialSharing) { }
 
   ngOnInit() {
   }
@@ -32,14 +34,14 @@ export class NoticiaComponent implements OnInit {
         icon: 'share',
         cssClass: 'action-dark',
         handler: () => {
-          console.log('Share clicked');
+          this.socialSharing.share(this.noticia.title, this.noticia.source.name, '', this.noticia.url);
         }
       }, {
         text: 'Favorito',
         icon: 'star',
         cssClass: 'action-dark',
         handler: () => {
-          console.log('Favorito');
+          console.log('Favoritos');
         }
       }, {
         text: 'Cancel',
