413b5928954f301205b922f3964333ee82412813
Andres Felipe Torres <andres.torres@ceiba.com.co>
2020-01-15 11:42:26 -0500

    Clase 105: Estilos en action sheet

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from 'src/app/Interfaces/interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-noticia',
@@ -13,13 +14,46 @@ export class NoticiaComponent implements OnInit {
   @Input() indice: number;
 
   constructor(
-    private inAppBrowser: InAppBrowser) { }
+    private inAppBrowser: InAppBrowser,
+    public actionSheetController: ActionSheetController) { }
 
   ngOnInit() {
   }
 
   abrirNoticia() {
     const browser = this.inAppBrowser.create(this.noticia.url, '_system');
+  }
+
+  async lanzarMenu() {
+
+    const actionSheet = await this.actionSheetController.create({
+      buttons: [{
+        text: 'Compartir',
+        icon: 'share',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Favorito',
+        icon: 'star',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Favorito');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+
   }
 
 }
